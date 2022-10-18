147fd129b825eafb1e6ac7aaa805e6e6ac50ead1
Yanina Martinez Villaverde <yaninamartinez340@gmail.com>
2022-10-18 16:50:27 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'holamundo-ionic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
